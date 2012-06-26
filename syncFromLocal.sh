ec603057c5b690a13e7f18478393da274798e657
echoldman <echoldman@gmail.com>
2012-06-26 18:29:14 +0800

    Add 'Sel', 'defIF', 'defInit', 'impIF', 'impInit', 'pa', 'uiviewAnimation'. Rename 'p' to 'pr'.

--- syncFromLocal.sh
+++ syncFromLocal.sh
@@ -4,4 +4,4 @@ baseDirForScriptSelf=$(cd "$(dirname "$0")"; pwd)
 # echo "full path to currently executed script is : ${baseDirForScriptSelf}"
 
 rm -fr ${baseDirForScriptSelf}/*.sublime-snippet
-cp ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/*.sublime-snippet ${baseDirForScriptSelf}
+cp ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/*.sublime-snippet ./
